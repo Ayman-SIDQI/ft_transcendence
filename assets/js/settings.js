35b1b0d4c9573b4e80e28b12398918dd7954c018
ouaZAK <zakariaouaraqa@gmail.com>
2024-06-27 16:43:14 +0100

    uplaod iconpic too in username

--- assets/js/settings.js
+++ assets/js/settings.js
@@ -1,6 +1,7 @@
 
 let profilepic = document.getElementById("profile-pic");
 let inputfile = document.getElementById("input-file");
+let iconpic = document.getElementById("icon-pic");
 
 if (profilepic && profilepic.style) {
 // profilepic.style.height = '80px';
@@ -15,7 +16,10 @@ profilepic.style.marginright = 'auto';
 profilepic.style.width = '250px';
 profilepic.style.height = '250px';
 profilepic.style.borderRadius = '50%';
+
+iconpic.style.borderRadius = '50%';
 }
 inputfile.onchange = function(){
 profilepic.src = URL.createObjectURL(inputfile.files[0]);
+iconpic.src = URL.createObjectURL(inputfile.files[0]);
 }
