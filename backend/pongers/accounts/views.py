4e5ca9886e1b67d286c60db597eb33c9c313594c
hafidara <hafidarahmane1@gmail.com>
2024-09-16 17:08:33 +0100

    login almost done, still needs cookies handling

--- backend/pongers/accounts/views.py
+++ backend/pongers/accounts/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
 from .serializers import UserSerializer
+from django.contrib.auth import authenticate
 
 class RegisterView(APIView):
     def post(self, request):
@@ -12,4 +13,13 @@ class RegisterView(APIView):
             if user:
                 return Response(serializer.data, status=status.HTTP_201_CREATED)    
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class LoginView(APIView):
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        user = authenticate(username=username, password=password)
+        if user:
+            return Response({'good': 'good'}, status=status.HTTP_200_OK)
+        return Response({'error': 'Invalid Credentials'},status=status.HTTP_400_BAD_REQUEST)
 
