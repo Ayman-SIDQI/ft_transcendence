3707379ea89049b50e4f65d56c0d9ba94d24aff6
Suigetsu <lagmed.019@gmail.com>
2024-09-16 13:53:17 +0100

    update

--- backend/pongers/accounts/views.py
+++ backend/pongers/accounts/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
-from django.contrib.auth.models import User
 from .serializers import UserSerializer
 
 class RegisterView(APIView):
