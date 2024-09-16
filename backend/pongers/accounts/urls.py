80a6388a71d5ee27747b7af893b0f98570aeb3ba
hafidara <hafidarahmane1@gmail.com>
2024-09-16 11:05:40 +0100

    new branch yay

--- backend/pongers/accounts/urls.py
+++ backend/pongers/accounts/urls.py
@@ -1,3 +1,7 @@
 from django.urls import path
-from . import views
+from .views import RegisterView
+
+urlpatterns = [
+    path('register/', RegisterView.as_view(), name='register'),
+]
 
--- backend/pongers/accounts/views.py
+++ backend/pongers/accounts/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
+from django.contrib.auth.models import User
+from .serializers import UserSerializer
+
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            if user:
+                return Response(serializer.data, status=status.HTTP_201_CREATED)    
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
