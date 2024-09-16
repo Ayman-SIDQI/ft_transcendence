e561faadd27dd97a36d99ce338c3b5783b6218eb
Suigetsu <lagmed.019@gmail.com>
2024-09-16 13:36:27 +0100

    fixed the email duplication

--- backend/pongers/accounts/serializers.py
+++ backend/pongers/accounts/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from rest_framework.validators import UniqueValidator
 
 class UserSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(
+        validators=[UniqueValidator(queryset=User.objects.all())]
+    )
+    username = serializers.CharField(
+        validators=[UniqueValidator(queryset=User.objects.all())]
+    )
     password = serializers.CharField(write_only=True)
 
     class Meta:
