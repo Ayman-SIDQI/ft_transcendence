45721cfb93054a3b92d33ed01655c500cdc9ff8d
Suigetsu <lagmed.019@gmail.com>
2024-09-16 15:05:02 +0100

    added password confirm case

--- backend/pongers/accounts/serializers.py
+++ backend/pongers/accounts/serializers.py
@@ -10,12 +10,19 @@ class UserSerializer(serializers.ModelSerializer):
         validators=[UniqueValidator(queryset=User.objects.all())]
     )
     password = serializers.CharField(write_only=True)
+    password_confirm = serializers.CharField(write_only=True)
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password')
+        fields = ('username', 'email', 'password', 'password_confirm')
+    
+    def validate(self, data):
+        if data['password'] != data['password_confirm']:
+            raise serializers.ValidationError({"password_confirm": "Passwords do not match."})
+        return data
     
     def create(self, validated_data):
+        validated_data.pop('password_confirm', None)
         user = User.objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
