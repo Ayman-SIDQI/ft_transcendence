8c07a84c0ae83b6916c74f222abc8821e8cf5e30
hafidara <hafidarahmane1@gmail.com>
2024-09-20 16:51:10 +0100

    delete account still not working, gotta consume more argan oil

--- backend/pongers/accounts/urls.py
+++ backend/pongers/accounts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import RegisterView, LoginView, set_csrf_token, LogoutView
+from .views import RegisterView, LoginView, GetCSRFToken, LogoutView, DeleteAccountView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
-    path('set-csrf-token/', set_csrf_token, name='set-csrf-token'),
+    path('csrf_token/', GetCSRFToken.as_view()),
+    path('delete-account/', DeleteAccountView.as_view(), name='delete-account'),
 ]
 
--- backend/pongers/accounts/views.py
+++ backend/pongers/accounts/views.py
@@ -1,14 +1,23 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework import status
+from rest_framework import status, permissions
 from .serializers import UserSerializer
 from django.contrib.auth import authenticate, login, logout
-from django.views.decorators.csrf import ensure_csrf_cookie
+from django.views.decorators.csrf import ensure_csrf_cookie, csrf_protect
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from django.utils.decorators import method_decorator
 
-@ensure_csrf_cookie
-def set_csrf_token(request):
-    return Response({'success': 'CSRF cookie set'})
+# @ensure_csrf_cookie
+# def set_csrf_token(request):
+#     return Response({'success': 'CSRF cookie set'})
+
+@method_decorator(ensure_csrf_cookie, name='dispatch')
+class GetCSRFToken(APIView):
+    permission_classes = (permissions.AllowAny, )
+    def get(self, request):
+        return Response({'success': 'CSRF cookie set'})
 
 class RegisterView(APIView):
     def post(self, request):
@@ -20,17 +29,21 @@ class RegisterView(APIView):
                 return Response(serializer.data, status=status.HTTP_201_CREATED)    
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+@method_decorator(csrf_protect, name='dispatch')
 class LoginView(APIView):
+    permission_classes = (permissions.AllowAny, )
     def post(self, request):
         username = request.data.get('username')
         password = request.data.get('password')
+        # print(username)
+        # print(password)
         if username is None or password is None:
             return Response({
                 "error": "Please provide both username and password"
             })
         user = authenticate(username=username, password=password)
         if user:
-            return Response({'good': 'good'}, status=status.HTTP_200_OK)
+            return Response({'success': 'good'}, status=status.HTTP_200_OK)
         return Response({'error': 'Invalid Credentials'},status=status.HTTP_400_BAD_REQUEST)
 
 
@@ -38,3 +51,17 @@ class LogoutView(APIView):
     def post(self, request):
         logout(request)
         return Response({'success': 'User logged out successfully!'}, status=status.HTTP_200_OK)
+
+
+class DeleteAccountView(APIView):
+    # permission_classes = [permissions.IsAuthenticated]
+    permission_classes = (permissions.AllowAny, )
+    @method_decorator(csrf_protect)
+    def delete(self, request, format=None):
+        user = self.request.user
+        print(request.data.get('username'))
+        try:
+            user.delete()
+            return Response({'success': 'Account deleted successfully!'}, status=status.HTTP_200_OK)
+        except:
+            return Response({'error': 'Unable to delete.'}, status=status.HTTP_400_BAD_REQUEST)
